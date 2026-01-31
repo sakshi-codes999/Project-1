14609f5c9665e1df675a278b6e12e9701b12ac4b
sakshi-codes999 <sakshirathi99972@gmail.com>
2026-01-31 21:01:52 +0530

    shopping website

---
+++ ecomm.js
@@ -0,0 +1,15 @@
+/*to make nav swap from horizontal to vertical */
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+  bar.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
+}
+if (close) {
+  close.addEventListener('click', () => {
+    nav.classList.remove('active');
+  });
+}
